7cea448b5a37a1b36320c7d1d332682d6bce5334
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-06-27 15:01:20 +0530

    delete smartphone

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddSmartPhone from './components/AddSmartPhone';
 import SearchSmartPhone from './components/SearchSmartPhone';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DeleteSmartPhone from './components/DeleteSmartPhone';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
    <Routes>
     <Route path="/" element={<AddSmartPhone/>}/>
     <Route path="/search" element={<SearchSmartPhone/>}/>
+    <Route path="/delete" element={<DeleteSmartPhone/>}/>
    </Routes>
    </BrowserRouter>
 
