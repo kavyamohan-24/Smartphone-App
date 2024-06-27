131ed3131e9c17d227c7caa943ebbe890993b628
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-06-27 14:24:55 +0530

    smartphone app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddSmartPhone from './components/AddSmartPhone';
+
+function App() {
+  return (
+    <div>
+      <AddSmartPhone/>
+    </div>
+  );
+}
+
+export default App;
