e693b7f4c4dab209f8bfc119b8f043c6962201dc
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-06-27 14:50:02 +0530

    search smartphone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSmartPhone from './components/AddSmartPhone';
+import SearchSmartPhone from './components/SearchSmartPhone';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddSmartPhone/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<AddSmartPhone/>}/>
+    <Route path="/search" element={<SearchSmartPhone/>}/>
+   </Routes>
+   </BrowserRouter>
+
+
   );
 }
 
